8655c57e3449f451ac6bbcf6c2ab5bf64b6e0349
hushbrush <harshitachakravadhanula@gmail.com>
2024-09-22 15:10:27 -0400

    still cors

--- stamp/main.js
+++ stamp/main.js
@@ -1,48 +1,61 @@
 
-//step 1: find the dominant colour
-//step 2: display it in a rectangle vertically half of the stamp
-//step 3: figure out how we'll filter it after that. That's step 3. Don't get ahead of yourself. Just do step 1 and 2 for now. You're good. chill chill chill chill chill chill chill.
+//cors issue:(
+//okay. leave that. Barring that, let's see what else
+//make it all the same height.
+//figure out modulartiy first
+//filtering:
+//1. store the colour in the object
+//2. add another tag in the object that in words tells you what the colour is.
 
+const apiKey = 'UIlZKqadOeQus4jccmxUP9WIqyEBKgZw9cfGghuk'; 
+const contentApiUrl = `https://api.si.edu/openaccess/api/v1.0/content/`;
+const searchApiUrl ="https://api.si.edu/openaccess/api/v1.0/search";
+let ID;
 
-   const apiKey = 'UIlZKqadOeQus4jccmxUP9WIqyEBKgZw9cfGghuk'; 
-   const contentApiUrl = `https://api.si.edu/openaccess/api/v1.0/content/`;
-   const searchApiUrl ="https://api.si.edu/openaccess/api/v1.0/search";
-   let ID;
-
-   let idArray = [];
+let idArray = [];
 let jsonString = '';
+const colorRanges = {
+  red:    { min: [200, 0, 0], max: [255, 100, 100] },
+  orange: { min: [200, 100, 0], max: [255, 165, 0] },
+  yellow: { min: [200, 200, 0], max: [255, 255, 100] },
+  green:  { min: [0, 200, 0], max: [100, 255, 100] },
+  blue:   { min: [0, 0, 200], max: [100, 100, 255] },
+  violet: { min: [100, 0, 200], max: [200, 100, 255] },
+  black:  { min: [0, 0, 0], max: [50, 50, 50] },
+  white:  { min: [200, 200, 200], max: [255, 255, 255] },
+};
 
-   //search api
-   function searchApi()
-   {
-    let url = searchApiUrl + "?api_key=" + apiKey + "&q=" + "online_visual_material:true AND type:edanmdm AND U.S. Stamps";
+
+function searchApi()
+{
+let url = searchApiUrl + "?api_key=" + apiKey + "&q=" + "online_visual_material:true AND type:edanmdm AND U.S. Stamps";
+
+window
+.fetch(url)
+.then(res => res.json())
+.then(data => {
+console.log(data)
+  
+
+  let pageSize = 10;
+  let numberOfQueries = 1;
+    //Math.ceil(data.response.rowCount / pageSize);//not working with all the data rn
+  console.log(numberOfQueries)
+  for(let i = 0; i < numberOfQueries; i++) {
+    if (i == (numberOfQueries - 1)) {
     
-    window
-    .fetch(url)
-    .then(res => res.json())
-    .then(data => {
-    console.log(data)
-      
-    
-      let pageSize = 1000;
-      let numberOfQueries = 5;
-       //Math.ceil(data.response.rowCount / pageSize);
-      console.log(numberOfQueries)
-      for(let i = 0; i < numberOfQueries; i++) {
-        if (i == (numberOfQueries - 1)) {
-        
-          searchAllURL = url + `&start=${i * pageSize}&rows=${data.response.rowCount - (i * pageSize)}`;
-        } else {
-          searchAllURL = url + `&start=${i * pageSize}&rows=${pageSize}`;
-        }
-        console.log(searchAllURL)
-        fetchAllData(searchAllURL);
-      
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    })
+      searchAllURL = url + `&start=${i * pageSize}&rows=${data.response.rowCount - (i * pageSize)}`;
+    } else {
+      searchAllURL = url + `&start=${i * pageSize}&rows=${pageSize}`;
+    }
+    console.log(searchAllURL)
+    fetchAllData(searchAllURL);
+  
+  }
+})
+.catch(error => {
+  console.log(error);
+})
 }
 
 // fetching all the data listed under our search and pushing them all into our custom array
@@ -62,11 +75,16 @@ function fetchAllData(url) {
     let counter=0;
     if (idArray.length > 0) {
       sortYear(idArray);
-      for (let i = 0; i < 10; i++) 
+      for (let i = 0; i < 5; i++) 
       {
-       for(let j=0; j<10; j++)
+       for(let j=0; j<2; j++)
         {
-          displayImage(idArray[counter].imageLink, i*160, j*110 );
+          let x=i*160;
+          let y=j*150;
+          displayImage(idArray[counter].imageLink, x, y );
+          findColor(counter, idArray[counter].imageLink );
+          displaycolor(counter, x,y );
+          //tagColor(idArray[index].color)
           
           counter++;
         }
@@ -81,23 +99,18 @@ function fetchAllData(url) {
 
 
 function addObject(objectData) {  
-  
-
-  let currentPlace = "";
-  if(objectData.content.indexedStructured.place) {
+if(objectData.content.indexedStructured.place) {
     currentPlace = objectData.content.indexedStructured.place[0];
   }
-  let date = "";
-  if(objectData.content.freetext.date) {
+  
+if(objectData.content.freetext.date) {
     stamp_date = objectData.content.freetext.date[0].content;
   }
- img_link = objectData.content.descriptiveNonRepeating.online_media.media[0].guid;
-// Convert to HTTPS if necessary
-// if (img_link.startsWith('http:')) {
-//   img_link = img_link.replace('http:', 'https:');
-// }
+  
+img_link = objectData.content.descriptiveNonRepeating.online_media.media[0].guid;
 
-idArray.push({
+if(img_link){
+  idArray.push({
     id: objectData.id,
     title: objectData.title,
     imageLink: img_link,
@@ -105,67 +118,85 @@ idArray.push({
   })
 }
 
+}
 const svg = d3.select('svg');
-
 searchApi();
 
    
 function displayImage(imageUrl, x, y) {
-  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  fetch(proxyUrl + imageUrl, {
-    method: 'GET',
-    mode: 'no-cors',
-    credentials: 'include',
-    headers: {
-      'Accept': 'application/json',  // example header
-      'Content-Type': 'application/json' // example header
-    }
-  })
-  
-  .then(response => {
-      if (!response.ok) {
-          throw new Error('Network response was not ok');
+            // Append image directly to SVG
+          svg.append('image')
+              .attr('x', x+20)
+              .attr('y', y)
+              .attr('height', 110) // Adjust size
+              //.attr('width', 160) // allows to render in height but without the 
+              .attr('href', imageUrl); // Use URL directly in SVG
+
+        
       }
-      return response.blob(); // Get the response as a Blob
-  })
-  .then(blob => {
-      const img = new Image();
-      img.src = URL.createObjectURL(blob); // Create an object URL for the Blob
+   
 
-      img.onload = function() {
-          // Getting dominant colour
-          Vibrant.from(img).getPalette((err, palette) => {
-              if (err) {
-                  console.error("Error getting color palette:", err);
-                  return;
-              }
 
-              const vibrantColor = palette.Vibrant.getHex();
-              const mutedColor = palette.Muted.getHex();
 
-              svg.append('image')
-                  .attr('x', x)
-                  .attr('y', y)
-                  .attr('width', img.width / 10) // Adjust if needed
-                  .attr('height', img.height / 10) // Adjust if needed
-                  .attr('href', img.src); // Use 'href' for image source in SVG
-
-              svg.append('rect')
-                  .attr('x', x) // X coordinate of the rectangle (same as image)
-                  .attr('y', y) // Y coordinate of the rectangle (same as image)
-                  .attr('width', (img.width / 10) / 2) // Half the width of the image
-                  .attr('height', img.height / 10) // Same height as the image
-                  .attr('fill', vibrantColor) // Fill color
-                  .attr('stroke', mutedColor) // Optional stroke
-                  .attr('stroke-width', 2); // Optional stroke width
-          });
-      };
-  })
-  .catch(error => {
-      console.error('Error loading image:', error);
-  });
+function displaycolor(index, x, y)
+{ 
+  svg.append('rect')
+  .attr('x', x) // X coordinate of the rectangle (same as image)
+  .attr('y', y) // Y coordinate of the rectangle (same as image)
+  .attr('width', 20) // Half the width of the image
+  .attr('height', 110) // Same height as the image
+  .attr('fill', 'blue')
+ // .attr('fill', `rgba(${idArray[index].color[0]}, ${idArray[index].color[1]}, ${idArray[index].color[2]}, ${idArray[index].color[3]})`)//might have to change this if the colour comes in hex eventually.
+ 
 }
 
+
+function findColor(index, imageUrl)
+{
+
+  //this stuff will give me the real colour but is flagging cors issues, working without that for now
+  //Using Vibrant directly with the image URL
+  const img = new Image();
+  img.crossOrigin = 'Anonymous';
+  img.src = imageUrl; // + '?not-from-cache-please';
+
+  const v = new Vibrant(img);
+  console.log(v.getPalette());
+  //  Vibrant.from(imageUrl)
+  //     .getPalette((err, palette) => {
+  //       console.log('in get palette')
+
+  //         if (err) {
+  //             console.error("Error getting color palette:", err);
+  //             return;
+  //         }
+
+          // Extract dominant and muted colors
+          //const vibrantColor =  [Math.ceil(Math.random()*255), Math.ceil(Math.random()*255), Math.ceil(Math.random()*255), 1]; //palette.Vibrant.getHex();
+          //const mutedColor =  palette.Muted.getHex();
+         // idArray[index].color = vibrantColor; // Add the color property
+       
+      //   })
+      //  }
+}
+
+    
+
+function tagColor(color)
+{
+  for (const [name, range] of Object.entries(colorRanges)) {
+    const [r, g, b] = color;
+
+    if (
+        r >= range.min[0] && r <= range.max[0] &&
+        g >= range.min[1] && g <= range.max[1] &&
+        b >= range.min[2] && b <= range.max[2]
+    ) {
+        return name;
+    }
+}
+return "unknown"; // or any default value
+}
 
 
 
